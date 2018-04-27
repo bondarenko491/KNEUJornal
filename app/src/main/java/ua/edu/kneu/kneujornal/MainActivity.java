270cc95843400d7d8e3933aea812bdc1daa2b301
Debug <12@dd>
2018-04-27 20:02:15 +0300

    fix main activity

--- app/src/main/java/ua/edu/kneu/kneujornal/MainActivity.java
+++ app/src/main/java/ua/edu/kneu/kneujornal/MainActivity.java
@@ -1,16 +1,11 @@
 package ua.edu.kneu.kneujornal;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,14 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
         return true;
     }
 
@@ -43,5 +36,4 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
-
 }
