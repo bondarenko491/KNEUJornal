c9f5e5b1e1641fd8c4483184452cb9d6863691ec
Debug <12@dd>
2018-05-02 00:01:57 +0300

    Login Form fix

--- app/src/main/java/ua/edu/kneu/kneujornal/MainActivity.java
+++ app/src/main/java/ua/edu/kneu/kneujornal/MainActivity.java
@@ -1,6 +1,9 @@
 package ua.edu.kneu.kneujornal;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,11 +11,17 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private String token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences mSettings;
+        mSettings = getPreferences(Context.MODE_PRIVATE);
+        token = mSettings.getString("token","");
+        if(token.isEmpty()) {
+            startActivity(new Intent(MainActivity.this,LoginActivity.class));
+        }
     }
 
     @Override
@@ -30,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
                 break;
             }
             case R.id.action_sign_out: {
+                SharedPreferences mSettings;
+                mSettings = getPreferences(Context.MODE_PRIVATE);
+                mSettings.edit().remove("token");
                 startActivity(new Intent(MainActivity.this,LoginActivity.class));
                 break;
             }
